c45b17cc496c233e33f84c4e7be8c0928317aa32
David L. Adei <lokingdav@gmail.com>
2025-02-16 23:03:05 +0000

    Improved infra management script

--- automation/infras.sh
+++ automation/infras.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e  # Exit immediately if any command fails
 
 cmd=$1
-cmds=('configure' 'create' 'destroy')
+cmds=('configure' 'create' 'destroy' 'install' 'run')
 
 configure() {
     ./scripts/generate-ssh-keys.sh
@@ -18,6 +19,28 @@ destroy() {
     terraform destroy
 }
 
+install() {
+    ansible-playbook -i hosts.yml playbooks/install.yml
+}
+
+runapp() {
+    local app=$1
+    case "$app" in
+        cpex)
+            tags="stop_cpex,start_cpex"
+            ;;
+        atis)
+            tags="stop_atis,start_atis"
+            ;;
+        *)
+            echo "Unknown app: $app"
+            exit 1
+            ;;
+    esac
+    
+    ansible-playbook -i hosts.yml playbooks/run.yml --tags "$tags"
+}
+
 case "$cmd" in
     configure)
         configure
@@ -27,6 +50,17 @@ case "$cmd" in
         ;;
     destroy)
         destroy
+        ;;
+    install)
+        install
+        ;;
+    run)
+        # Ensure an app name is provided
+        if [ -z "$2" ]; then
+            echo "Usage: $0 run {cpex|atis}"
+            exit 1
+        fi
+        runapp "$2"
         ;;
     *)
         echo "Usage: $0 {${cmds[*]}}"
