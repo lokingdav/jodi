d54fe0c9ab38486ef7d1bbe9cdb112df9d5db987
David L. Adei <lokingdav@gmail.com>
2025-02-17 21:05:17 +0000

    Temporary disable key rotation

--- cpex/servers/keyrotation.py
+++ cpex/servers/keyrotation.py
@@ -5,7 +5,7 @@ cache.set_client(cache.connect())
 
 def main():
     KeyRotation.initialize_keys()
-    KeyRotation.begin_rotation()
+    # KeyRotation.begin_rotation()
 
 if __name__ == '__main__':
     main()
