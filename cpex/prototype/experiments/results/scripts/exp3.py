f63142e9283aad08e77267562eefd7f8b70b22bc
David L. Adei <lokingdav@gmail.com>
2025-02-17 21:05:52 +0000

    WIP: analyzing results for exp3

---
+++ cpex/prototype/experiments/results/scripts/exp3.py
@@ -0,0 +1,7 @@
+from cpex.prototype.experiments.results.scripts import helpers
+import numpy as np
+
+
+if __name__ == "__main__":
+    file_path = "cpex/prototype/experiments/results/experiment-3.csv"
+    columns = ['calls_count', 'lat_mean']
