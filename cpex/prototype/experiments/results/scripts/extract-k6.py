07e994db8181b0f5c0986b01d71845ec2d79a87b
David L. Adei <lokingdav@gmail.com>
2025-02-23 08:34:55 +0000

    Added load testing results

---
+++ cpex/prototype/experiments/results/scripts/extract-k6.py
@@ -0,0 +1,27 @@
+import os, json
+from cpex.helpers import files
+
+folder = 'cpex/prototype/experiments/results/k6'
+
+def main():
+    filenames = os.listdir(folder)
+    rows = []
+    dp = 3
+    for file in filenames:
+        if file.endswith('.json'):
+            with open(f'{folder}/{file}', 'r') as f:
+                data = json.load(f)
+                (prot, _) = file.split('-')
+                rows.append([
+                    prot, 
+                    data['metrics']['vus']['max'], 
+                    round(data['metrics']['http_req_duration']['med'], dp),
+                    round(data['metrics']['iterations']['rate'], dp),
+                ])
+
+    rows.sort(key=lambda x: x[0])
+    rows = [['Protocol', 'VUs', 'Median']] + rows
+    files.write_csv('cpex/prototype/experiments/results/k6.csv', rows)
+
+if __name__ == '__main__':
+    main()
--- runk6.sh
+++ runk6.sh
@@ -8,7 +8,7 @@ if [ -z "$protocol" ]; then
     exit 1
 fi
 
-rest_time=2
+rest_time=35
 x=(20 40 60 100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 850 900 950 1000)
 lastval=${x[${#x[@]}-1]}
 
@@ -18,7 +18,7 @@ for i in "${x[@]}" ; do
 
     k6 run --log-output=stdout \
         --summary-export "cpex/prototype/experiments/results/k6/$protocol-$i.json" \
-        --vus $i
+        --vus $i \
         --duration 1m \
         "cpex/prototype/experiments/k6/$protocol.js"
 
