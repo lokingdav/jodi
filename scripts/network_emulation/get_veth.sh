f5936c6bf20eb7a0ba358bdefdb8dc02f06b9d13
nithinshyam13 <34136710+nithinshyam13@users.noreply.github.com>
2025-05-04 06:41:51 -0400

    Delete scripts/network_emulation directory

--- scripts/network_emulation/get_veth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for containername in $(docker ps --format "{{.Names}}"); do
-    iflink=`docker exec -it $containername bash -c 'cat /sys/class/net/eth0/iflink'`
-    iflink=`echo $iflink|tr -d '\r'`
-    veth=`grep -l $iflink /sys/class/net/veth*/ifindex`
-    veth=`echo $veth|sed -e 's;^.*net/\(.*\)/ifindex$;\1;'`
-    echo $containername:$veth
-done
