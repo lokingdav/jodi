cc0ba532c622a4b75802d873e2ed10600ea39474
dahmed <dahmed@ncsu.edu>
2025-06-05 08:58:14 -0400

    Added req and res sizes trend

--- jodi/prototype/experiments/k6/ev.js
+++ jodi/prototype/experiments/k6/ev.js
@@ -1,19 +1,30 @@
 import http from 'k6/http';
 import { SharedArray } from 'k6/data';
 import { sleep, check } from 'k6';
+import { Trend } from 'k6/metrics';
 
 const items = new SharedArray('items', function () {
   const content = JSON.parse(open('../../../../conf/loads.json'));
   return content;
 });
 
+let reqSize = new Trend('cidgen_req_size');
+let resSize = new Trend('cidgen_res_size');
+
 export default function () {
-    const i = Math.floor(Math.random() * items.length);
+    const record = items[Math.floor(Math.random() * items.length)];
     const host = __ENV.HOST || 'evaluator';
     const url = `http://${host}/evaluate`;
-    const body = JSON.stringify(items[i].jodi.oprf);
+    const body = JSON.stringify({
+        ...record.jodi.oprf,
+        bt: record.jodi.bt,
+        peers: record.jodi.evs_peers,
+    });
     const params = { headers: {'Content-Type': 'application/json'} };
     const res = http.post(url, body, params);
-    // console.log(res.json());
+    
+    reqSize.add(res.request.body.length);
+    resSize.add(res.body.length);
+
     sleep(Math.random());
 }
--- jodi/prototype/experiments/k6/ms.js
+++ jodi/prototype/experiments/k6/ms.js
@@ -1,31 +1,44 @@
 import http from 'k6/http';
 import { SharedArray } from 'k6/data';
 import { sleep, check } from 'k6';
+import { Trend } from 'k6/metrics';
 
 const items = new SharedArray('items', function () {
   const content = JSON.parse(open('../../../../conf/loads.json'));
   return content;
 });
 
+let pubReqSize = new Trend('pub_req_size');
+let pubResSize = new Trend('pub_res_size');
+
+let retReqSize = new Trend('ret_req_size');
+let retResSize = new Trend('pub_res_size');
+
 export default function () {
-    const i = Math.floor(Math.random() * items.length);
+    const record = items[Math.floor(Math.random() * items.length)];
     const host = __ENV.HOST || 'message-store';
     const params = { headers: {'Content-Type': 'application/json'} };
 
     if (Math.random() < 0.5) {
         const res = http.post(`http://${host}/publish`, JSON.stringify({
-            idx: items[i].jodi.idx, 
-            ctx: items[i].jodi.ctx, 
-            sig: items[i].jodi.pub_sig
+            idx: record.jodi.idx, 
+            ctx: record.jodi.ctx, 
+            sig: record.jodi.pub_sig,
+            bt: record.jodi.bt,
+            peers: record.jodi.mss_peers,
         }), params);
-        // console.log(res.body);
+        pubReqSize.add(res.request.body.length);
+        pubResSize.add(res.body.length);
     } else {
         const res = http.post(`http://${host}/retrieve`, JSON.stringify({
-            idx: items[i].jodi.idx, 
-            sig: items[i].jodi.ret_sig
+            idx: record.jodi.idx, 
+            sig: record.jodi.ret_sig,
+            bt: record.jodi.bt,
+            peers: record.jodi.mss_peers,
         }), params);
-        // console.log(res.body);
+        retReqSize.add(res.request.body.length);
+        retResSize.add(res.body.length);
     }
     
-    sleep(Math.random());
+    sleep(0.15);
 }
